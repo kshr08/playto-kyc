9c1e1dfacce61f91dcf2a3524f9da2a8b63f44ee
Shravani Khindre <shravanikhindre@gmail.com>
2026-04-25 17:18:19 +0530

    before production fix

--- backend/kyc_service/settings.py
+++ backend/kyc_service/settings.py
@@ -9,7 +9,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
